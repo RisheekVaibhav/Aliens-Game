1c1ca26ab9b621723afbb7a506f3a05f1641e50d
Risheek <risheekjk@gmail.com>
2025-11-23 04:34:32 +0530

    Commit - 3
    Added bullets to the ship and adjusted their speed. Also made sure when bullets leave the screen they are deleted.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullets import Bullet
 
 class AlienInvasion:
     def __init__(self):
@@ -17,16 +18,25 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 2:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
             self._update_screen()
             self.clock.tick(60)
 
     def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             self.ship.blitme()
             pygame.display.flip()
         
@@ -34,15 +44,21 @@ class AlienInvasion:
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
-                self.ship.moving_left = True
+            self.ship.moving_left = True
         elif event.key == pygame.K_q:
-             sys.exit()
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self.fire_bullet()
         
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
                           
          
     def _check_events(self):
